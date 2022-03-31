efc28d15ebb1e437841c91f733932465f3de6af5
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-31 20:53:11 +0200

    Modification de la création de l'objet YouTube pour rajouter les fonctions appelées quand le téléchargement est en cours

--- scripts/download/download_video.py
+++ scripts/download/download_video.py
@@ -1,6 +1,16 @@
 from pytube import YouTube
 
-url = input("Donne-moi un lien de vidéo à télécharger !") # Demande le lien de la vidéo
-video = YouTube(url) # Crée l'objet Youtube correspondant à la vidéo
+# Définit les fonctions de progression du téléchargement
+def on_progress():
+    pass
+
+def on_complete():
+    pass
+
+# Demande le lien de la vidéo et la télécharge
+url = input("Donne-moi un lien de vidéo à télécharger ! ") # Demande le lien de la vidéo
+video = YouTube(url,
+                on_progress_callback=on_progress,
+                on_complete_callback=on_complete,) # Crée l'objet Youtube correspondant à la vidéo
 streams_progressive = video.streams.filter(progressive=True) # Récupère toutes les qualités possibles avec vidéo + audio (qualité max : 720p)
 streams_progressive.get_highest_resolution().download() # Télécharge la qualité la plus haute
