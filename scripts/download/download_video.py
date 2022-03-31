a3f391836339380bb391351a85b8699bfcd54bef
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-31 19:09:03 +0200

    Ajout du téléchargement pour la qualité la plus haute

--- scripts/download/download_video.py
+++ scripts/download/download_video.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
 
-video = YouTube("https://www.youtube.com/watch?v=q73RiLr9GHU")
+video = YouTube("https://www.youtube.com/watch?v=4bUBotqLdbc") # Crée l'objet Youtube correspondant à la vidéo
 streams_progressive = video.streams.filter(progressive=True) # Récupère toutes les qualités possibles avec vidéo + audio (qualité max : 720p)
+streams_progressive.get_highest_resolution().download() # Télécharge la qualité la plus haute
