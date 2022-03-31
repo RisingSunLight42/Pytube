cf437ee66ed076c3294c6928df5209880d55df40
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-31 20:29:42 +0200

    Ajout d'un input pour récupérer le lien

--- scripts/download/download_video.py
+++ scripts/download/download_video.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 
-video = YouTube("https://www.youtube.com/watch?v=4bUBotqLdbc") # Crée l'objet Youtube correspondant à la vidéo
+url = input("Donne-moi un lien de vidéo à télécharger !") # Demande le lien de la vidéo
+video = YouTube(url) # Crée l'objet Youtube correspondant à la vidéo
 streams_progressive = video.streams.filter(progressive=True) # Récupère toutes les qualités possibles avec vidéo + audio (qualité max : 720p)
 streams_progressive.get_highest_resolution().download() # Télécharge la qualité la plus haute
