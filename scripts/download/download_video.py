ea2fd8debc1996492264046a6ad6c3cec0d2c691
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-02 18:24:49 +0200

    Ajout du rename du fichier lorsque le fichier est un .mp3

--- scripts/download/download_video.py
+++ scripts/download/download_video.py
@@ -1,5 +1,6 @@
-from tkinter.filedialog import askdirectory
 from pytube import YouTube
+from os import rename
+from os.path import splitext
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 
@@ -22,6 +23,7 @@ def on_complete(stream, file_path):
         stream (stream object): objet contenant les différentes informations du fichier vidéo
         file_path (str): chemin d'accès vers le fichier vidéo
     """
+    file_path = file_path.split(".")[0] # Retire l'extension du fichier enregistré, pouvant être fausse
     print(f"La vidéo '{stream.title}' a bien été enregistrée dans le chemin ci-contre :\n{file_path}")
 
 # Demande le chemin d'accès où le fichier sera téléchargé
@@ -39,8 +41,11 @@ video = YouTube(url,
 audio_ou_video = input("Veux-tu télécharger uniquement un fichier AUDIO ou un fichier VIDEO (répondre par AUDIO ou VIDEO) ") # Demande ce que veut l'utilisateur
 if audio_ou_video == "AUDIO": # Si l'utilisateur a demandé un audio
     flux_audio_dl = video.streams.filter(only_audio=True)[0]
-    flux_audio_dl.download(filename=flux_audio_dl.default_filename.replace(" ", "_"),
+    sortie_dl = flux_audio_dl.download(filename=flux_audio_dl.default_filename.replace(" ", "_"),
                            output_path=path) # Télécharge le flux audio, en remplaçant les espaces du nom de fichier par des "_" au chemin d'accès spécifié
+    chemin, extension = splitext(sortie_dl) # splitext sépare le chemin d'accès du fichier et son extension
+    sortie_dl_rename = f"{chemin}.mp3" # Modifie le chemin pour mettre l'extension .mp3 et pas mp4
+    rename(sortie_dl, sortie_dl_rename) # Rename le fichier  avec le nouveau nom
 elif audio_ou_video == "VIDEO": # Si l'utilisateur a demandé une video
     streams_progressive = video.streams.filter(progressive=True) # Récupère toutes les qualités possibles avec vidéo + audio (qualité max : 720p)
     flux_video_dl = streams_progressive.get_highest_resolution() # Stocke le flux vidéo que l'on souhaite télécharger, avec la qualité la plus haute possible
