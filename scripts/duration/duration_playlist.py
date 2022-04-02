b872badf2c85b5fb53d5afc13c9d18f2ea508a8e
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-02 23:07:24 +0200

    Ajout de la boucle de calcul de la durée totale

--- scripts/duration/duration_playlist.py
+++ scripts/duration/duration_playlist.py
@@ -1,4 +1,9 @@
 from pytube import Playlist
 
-url = input("Donne moi le lien de la playlist !") # Demande le lien de la playlist
+url = input("Donne moi le lien de la playlist ! ") # Demande le lien de la playlist
 playlist = Playlist(url) # Crée l'objet playlist à partir du lien donné
+
+# Boucle sur les vidéos de la playlist pour calculer la durée totale de la playlist
+duree_totale = 0
+for video in playlist.videos:
+    duree_totale += video.length
