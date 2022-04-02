c463139265729a919eb410afd902515df0d94ed3
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-02 23:19:00 +0200

    Ajout du calcul de la durée en heure, minute et seconde

--- scripts/duration/duration_playlist.py
+++ scripts/duration/duration_playlist.py
@@ -7,3 +7,10 @@ playlist = Playlist(url) # Crée l'objet playlist à partir du lien donné
 duree_totale = 0
 for video in playlist.videos:
     duree_totale += video.length
+
+# Calcule la durée en heure, minute, seconde
+heure = duree_totale // 3600
+duree_totale -= (duree_totale // 3600) * 3600 # Retire les secondes correspondant au nombre d'heures de la durée totale
+minute = duree_totale // 60
+duree_totale -= (duree_totale // 60) * 60 # Retire les secondes correspondant au nombre de minutes de la durée totale
+seconde = duree_totale # La durée totale restante correspond aux secondes (variable crée pour une meilleure lisibilitée)
