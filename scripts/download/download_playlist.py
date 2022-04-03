8f457f42cb34ba170a3d5d42d50dfe3189913fe0
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-03 17:45:52 +0200

    Ajout des demandes à l'utilisateur

--- scripts/download/download_playlist.py
+++ scripts/download/download_playlist.py
@@ -1,5 +1,16 @@
 from pytube import Playlist
 from .download_video import telechargement
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
 
+# Demande le chemin d'accès où le fichier sera téléchargé
+root = Tk()
+root.wm_withdraw()
+root.iconify()
+path = askdirectory()
+root.destroy()
+
+# Demande le lien de la playlist et le format de téléchargement souhaité
 url = input("Donne moi le lien de la playlist ! ") # Demande le lien de la playlist
+audio_ou_video = input("Veux-tu télécharger uniquement les vidéos en AUDIO ou en VIDEO (répondre par AUDIO ou VIDEO) ") # Demande ce que veut l'utilisateur
 playlist = Playlist(url) # Crée l'objet playlist à partir du lien donné
