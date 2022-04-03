43ad600b8f925aa9a3012be52bba51046ba80ab5
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-03 17:33:18 +0200

    Déplacement du programme dans une fonction

--- scripts/download/download_video.py
+++ scripts/download/download_video.py
@@ -26,30 +26,33 @@ def on_complete(stream, file_path):
     file_path = file_path.split(".")[0] # Retire l'extension du fichier enregistré, pouvant être fausse
     print(f"La vidéo '{stream.title}' a bien été enregistrée dans le chemin ci-contre :\n{file_path}")
 
-# Demande le chemin d'accès où le fichier sera téléchargé
-root = Tk()
-root.wm_withdraw()
-root.iconify()
-path = askdirectory()
-root.destroy()
-
-# Demande le lien de la vidéo et la télécharge
-url = input("Donne-moi un lien de vidéo à télécharger ! ") # Demande le lien de la vidéo
-video = YouTube(url,
-                on_progress_callback=on_progress,
-                on_complete_callback=on_complete,) # Crée l'objet Youtube correspondant à la vidéo
-audio_ou_video = input("Veux-tu télécharger uniquement un fichier AUDIO ou un fichier VIDEO (répondre par AUDIO ou VIDEO) ") # Demande ce que veut l'utilisateur
-if audio_ou_video == "AUDIO": # Si l'utilisateur a demandé un audio
-    flux_audio_dl = video.streams.filter(only_audio=True)[0]
-    sortie_dl = flux_audio_dl.download(filename=flux_audio_dl.default_filename.replace(" ", "_"),
-                           output_path=path) # Télécharge le flux audio, en remplaçant les espaces du nom de fichier par des "_" au chemin d'accès spécifié
-    chemin, extension = splitext(sortie_dl) # splitext sépare le chemin d'accès du fichier et son extension
-    sortie_dl_rename = f"{chemin}.mp3" # Modifie le chemin pour mettre l'extension .mp3 et pas mp4
-    rename(sortie_dl, sortie_dl_rename) # Rename le fichier  avec le nouveau nom
-elif audio_ou_video == "VIDEO": # Si l'utilisateur a demandé une video
-    streams_progressive = video.streams.filter(progressive=True) # Récupère toutes les qualités possibles avec vidéo + audio (qualité max : 720p)
-    flux_video_dl = streams_progressive.get_highest_resolution() # Stocke le flux vidéo que l'on souhaite télécharger, avec la qualité la plus haute possible
-    flux_video_dl.download(filename=flux_video_dl.default_filename.replace(" ", "_"),
-                           output_path=path) # Télécharge le flux, en remplaçant les espaces du nom de fichier par des "_" au chemin d'accès spécifié
-else: # Si le mot clé ne colle pas
-    print("Le mot clé que tu m'as donné est incorrect !")
+def telechargement():
+    # Demande le chemin d'accès où le fichier sera téléchargé
+    root = Tk()
+    root.wm_withdraw()
+    root.iconify()
+    path = askdirectory()
+    root.destroy()
+    
+    # Demande le lien de la vidéo et le format souhaité
+    url = input("Donne-moi un lien de vidéo à télécharger ! ") # Demande le lien de la vidéo
+    audio_ou_video = input("Veux-tu télécharger uniquement un fichier AUDIO ou un fichier VIDEO (répondre par AUDIO ou VIDEO) ") # Demande ce que veut l'utilisateur
+    video = YouTube(url,
+                    on_progress_callback=on_progress,
+                    on_complete_callback=on_complete,) # Crée l'objet Youtube correspondant à la vidéo
+    
+    # Réalise le téléchargement
+    if audio_ou_video == "AUDIO": # Si l'utilisateur a demandé un audio
+        flux_audio_dl = video.streams.filter(only_audio=True)[0]
+        sortie_dl = flux_audio_dl.download(filename=flux_audio_dl.default_filename.replace(" ", "_"),
+                            output_path=path) # Télécharge le flux audio, en remplaçant les espaces du nom de fichier par des "_" au chemin d'accès spécifié
+        chemin, extension = splitext(sortie_dl) # splitext sépare le chemin d'accès du fichier et son extension
+        sortie_dl_rename = f"{chemin}.mp3" # Modifie le chemin pour mettre l'extension .mp3 et pas mp4
+        rename(sortie_dl, sortie_dl_rename) # Rename le fichier  avec le nouveau nom
+    elif audio_ou_video == "VIDEO": # Si l'utilisateur a demandé une video
+        streams_progressive = video.streams.filter(progressive=True) # Récupère toutes les qualités possibles avec vidéo + audio (qualité max : 720p)
+        flux_video_dl = streams_progressive.get_highest_resolution() # Stocke le flux vidéo que l'on souhaite télécharger, avec la qualité la plus haute possible
+        flux_video_dl.download(filename=flux_video_dl.default_filename.replace(" ", "_"),
+                            output_path=path) # Télécharge le flux, en remplaçant les espaces du nom de fichier par des "_" au chemin d'accès spécifié
+    else: # Si le mot clé ne colle pas
+        print("Le mot clé que tu m'as donné est incorrect !")
