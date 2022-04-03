7bd455dd8d8337d85123b4c8a5ef085909b735d4
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-03 17:47:29 +0200

    Ajout de la demande de la création d'un dossier

--- scripts/download/download_playlist.py
+++ scripts/download/download_playlist.py
@@ -13,4 +13,5 @@ root.destroy()
 # Demande le lien de la playlist et le format de téléchargement souhaité
 url = input("Donne moi le lien de la playlist ! ") # Demande le lien de la playlist
 audio_ou_video = input("Veux-tu télécharger uniquement les vidéos en AUDIO ou en VIDEO (répondre par AUDIO ou VIDEO) ") # Demande ce que veut l'utilisateur
+creation_dossier = input("Veux-tu avoir un dossier dédié à la playlist dans le dossier que tu as choisi ? Oui/Non ")
 playlist = Playlist(url) # Crée l'objet playlist à partir du lien donné
