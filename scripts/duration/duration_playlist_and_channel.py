b33c4086a7b73699b017f2522e5056df25031ce7
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-04 21:05:03 +0200

    Correction du message de correction

--- scripts/duration/duration_playlist_and_channel.py
+++ scripts/duration/duration_playlist_and_channel.py
@@ -1,6 +1,6 @@
 from pytube import Playlist, Channel
 
-playlist_or_channel = input("Veux-tu le temps de visionnage d'une playlist ou d'une chaîne Youtube ? playlist/channel ") # Demande ce que veut l'utilisateur
+playlist_or_channel = input("Veux-tu le temps de visionnage d'une playlist ou d'une chaîne Youtube ? (playlist/channel) ") # Demande ce que veut l'utilisateur
 if playlist_or_channel == "playlist":
     url = input("Donne moi le lien de la playlist ! ") # Demande le lien de la playlist
     objet = Playlist(url) # Crée l'objet playlist à partir du lien donné
@@ -24,5 +24,5 @@ def duree(duree_totale_seconde):
 
 duree_objet = duree(duree_totale) # Calcule la durée de la chaîne ou de la playlist
 duree_moy_video = duree(round(duree_totale / len(objet.videos))) # Calcule la durée moyenne d'une vidéo de la playlist
-print(f"La durée est de {duree_objet[0]}h{duree_objet[1]}m{duree_objet[2]}s.") # Informe de la durée totale de la playlist ou de la chaîne
+print(f"La durée totale de toutes les vidéos cumulées est de {duree_objet[0]}h{duree_objet[1]}m{duree_objet[2]}s.") # Informe de la durée totale de la playlist ou de la chaîne
 print(f"Sur les {len(objet.videos)} vidéos, une vidéo dure en moyenne :\n{duree_moy_video[0]}h{duree_moy_video[1]}m{duree_moy_video[2]}s")
