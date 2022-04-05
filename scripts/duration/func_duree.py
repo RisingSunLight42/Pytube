e5c3be4c7278c8723566426d1ba977d1aba8db87
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-05 20:56:57 +0200

    Ajout de la docstring

--- scripts/duration/func_duree.py
+++ scripts/duration/func_duree.py
@@ -2,6 +2,15 @@ from typing import Tuple
 
 # Fonction permettant de calculer la durée
 def duree(duree_totale_seconde: int) -> Tuple[int]:
+    """Fonction calculant l'équivalent en heure(s), minute(s), seconde(s)
+    d'une valeur de seconde(s)
+
+    Args:
+        duree_totale_seconde (int): valeur en seconde que l'on souhaite convertir
+
+    Returns:
+        Tuple[int]: sous la forme (heure, minute, seconde)
+    """
     heure = duree_totale_seconde // 3600
     duree_totale_seconde -= (duree_totale_seconde // 3600) * 3600 # Retire les secondes correspondant au nombre d'heures de la durée totale
     minute = duree_totale_seconde // 60
