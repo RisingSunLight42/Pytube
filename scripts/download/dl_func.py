5dc6eb0ba30075839291ff4ec21b21ab7901b1c6
RisingSunLight <risingsunlight5776@gmail.com>
2022-04-05 20:59:28 +0200

    Ajout d'annotation de types et mise en majuscule d'audio ou video

--- scripts/download/dl_func.py
+++ scripts/download/dl_func.py
@@ -24,7 +24,7 @@ def on_complete(stream, file_path):
     file_path = file_path.split(".")[0] # Retire l'extension du fichier enregistré, pouvant être fausse
     print(f"La vidéo '{stream.title}' a bien été enregistrée dans le chemin ci-contre :\n{file_path}")
 
-def telechargement(path, url, audio_ou_video):
+def telechargement(path: str, url: str, audio_ou_video: str) -> None:
     """Fonction permettant de télécharger une vidéo.
 
     Args:
@@ -37,14 +37,14 @@ def telechargement(path, url, audio_ou_video):
                     on_complete_callback=on_complete,) # Crée l'objet Youtube correspondant à la vidéo
     
     # Réalise le téléchargement
-    if audio_ou_video == "AUDIO": # Si l'utilisateur a demandé un audio
+    if audio_ou_video.upper() == "AUDIO": # Si l'utilisateur a demandé un audio
         flux_audio_dl = video.streams.filter(only_audio=True)[0]
         sortie_dl = flux_audio_dl.download(filename=flux_audio_dl.default_filename.replace(" ", "_"),
                             output_path=path) # Télécharge le flux audio, en remplaçant les espaces du nom de fichier par des "_" au chemin d'accès spécifié
         chemin, extension = splitext(sortie_dl) # splitext sépare le chemin d'accès du fichier et son extension
         sortie_dl_rename = f"{chemin}.mp3" # Modifie le chemin pour mettre l'extension .mp3 et pas mp4
         rename(sortie_dl, sortie_dl_rename) # Rename le fichier  avec le nouveau nom
-    elif audio_ou_video == "VIDEO": # Si l'utilisateur a demandé une video
+    elif audio_ou_video.upper() == "VIDEO": # Si l'utilisateur a demandé une video
         streams_progressive = video.streams.filter(progressive=True) # Récupère toutes les qualités possibles avec vidéo + audio (qualité max : 720p)
         flux_video_dl = streams_progressive.get_highest_resolution() # Stocke le flux vidéo que l'on souhaite télécharger, avec la qualité la plus haute possible
         flux_video_dl.download(filename=flux_video_dl.default_filename.replace(" ", "_"),
